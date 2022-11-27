6286e8666b6cc565a8a829af38c82a4f68000f09
davidgfb <davidgracia99@gmail.com>
2022-11-27 13:24:35 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from shadertoy import ShaderToyWindow
+
+
+class Project(ShaderToyWindow):
+    resource_dir = Path(__file__).parent.resolve()
+    main_program = 'test.glsl'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wnd.position = 0, 0
+        self.wnd.size = 1000, 1440
+
+Project.run()
